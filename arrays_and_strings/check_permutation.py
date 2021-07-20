eb003cddf0e23a3a56e88b3c105efe0b6305896d
Joseph Campion <josephcampion@system76-pc.localdomain>
2021-07-19 23:40:11 -0400

    Redo is_unique in nlogn time with sorting

---
+++ arrays_and_strings/check_permutation.py
@@ -0,0 +1,4 @@
+
+
+def check_permutation(str1, str2):
+    
--- arrays_and_strings/is_unique.py
+++ arrays_and_strings/is_unique.py
@@ -15,6 +15,20 @@ print(is_unique("abba"))
 print(is_unique("abcdefa"))
 print(is_unique("a"))
 print(is_unique(""))
+print()
 
+def is_unique_nlogn(s):
+    s = ''.join(sorted(s))
+    print(s)
 
+    for i in range(len(s)-1):
+        if s[i] == s[i+1]:
+            return False
+    return True
+
+print(is_unique_nlogn("abcd"))
+print(is_unique_nlogn("abba"))
+print(is_unique_nlogn("abcdefa"))
+print(is_unique_nlogn("a"))
+print(is_unique_nlogn(""))
         
