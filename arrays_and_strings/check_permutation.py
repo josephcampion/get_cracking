ecb7c63f42f109bf2df056ea0c69a4988408c685
Joseph Campion <josephcampion@system76-pc.localdomain>
2021-07-20 00:26:48 -0400

    Add hash table implementation of check permutation

--- arrays_and_strings/check_permutation.py
+++ arrays_and_strings/check_permutation.py
@@ -1,4 +1,5 @@
 
+from hash_table import HashTable
 
 def check_permutation(str1, str2):
     if (len(str1) != len(str2)):
@@ -33,4 +34,52 @@ a1 = ""
 b1 = "cba"
 print(check_permutation(a1, b1))
 
+def check_permutation_ht(str1, str2):
+    print(str1, str2)
+
+    ht1 = create_ht(str1)
+    ht2 = create_ht(str2)
+    
+    for c in str1:
+        v1 = ht1.get_val(c)
+        v2 = ht2.get_val(c)
+        if v1 != v2:
+            return False
+
+    for c in str2:
+        v1 = ht1.get_val(c)
+        v2 = ht2.get_val(c)
+        if v1 != v2:
+            return False
+
+    return True
+    
+def create_ht(str):
+    ht = HashTable(25)
+
+    for c in str:
+        val = ht.get_val(c)
+        if val == "No record found":
+            ht.set_val(c, 1)
+        else:
+            ht.set_val(c, val+1)
+
+    return ht
+
+
+a1 = "abc"
+b1 = "cba"
+print(check_permutation_ht(a1, b1))
+
+a1 = "abcd"
+b1 = "cba"
+print(check_permutation_ht(a1, b1))
+
+a1 = "abc"
+b1 = "cbb"
+print(check_permutation_ht(a1, b1))
+
+a1 = ""
+b1 = "cba"
+print(check_permutation_ht(a1, b1))
 
