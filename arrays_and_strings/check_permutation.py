e5528de1d91585b7441ada145f3953d6ced56e23
Joseph Campion <josephcampion@system76-pc.localdomain>
2021-07-20 00:05:52 -0400

    Implement nlogn check_permutation and add hash table class

--- arrays_and_strings/check_permutation.py
+++ arrays_and_strings/check_permutation.py
@@ -1,4 +1,36 @@
 
 
 def check_permutation(str1, str2):
-    
+    if (len(str1) != len(str2)):
+        return False
+
+    str1 = ''.join(sorted(str1))
+    str2 = ''.join(sorted(str2))
+
+    print(str1)
+    print(str2)
+
+    for i in range(len(str1)):
+        if str1[i] != str2[i]:
+            return False
+
+    return True
+
+
+a1 = "abc"
+b1 = "cba"
+print(check_permutation(a1, b1))
+
+a1 = "abcd"
+b1 = "cba"
+print(check_permutation(a1, b1))
+
+a1 = "abc"
+b1 = "cbb"
+print(check_permutation(a1, b1))
+
+a1 = ""
+b1 = "cba"
+print(check_permutation(a1, b1))
+
+
